bb50a81a68ee95a989ee28346c50857f0d792cc7
Andrea Jasper <andrea.nicole.jasper@gmail.com>
2021-04-28 14:56:35 +0000

    Automatically backed up by Learn

--- src/components/Pikachu.js
+++ src/components/Pikachu.js
@@ -10,7 +10,7 @@ class Pikachu extends React.Component {
     };
   }
   
-  componentDidUpdate() {this.resizePikachu}
+  componentDidUpdate() {this.resizePikachu()}
   
   resizePikachu = () => {
     const pikachu = document.getElementById("pikachu");
